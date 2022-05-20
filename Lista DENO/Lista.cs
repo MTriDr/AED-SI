839ff97e0145f96c9d59416b28e4c33e7d2a43bf
Matheus Trindade Rocha <64276897+MTriDr@users.noreply.github.com>
2022-05-20 10:35:21 -0300

    Add files via upload

---
+++ Lista DENO/Lista.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WorkBJA
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            // Lista Duplamente Encadeada
+
+            Lista MinhaLista = new Lista();
+
+            MinhaLista.InserirInício(4);
+
+            MinhaLista.InserirInício(19);
+
+            MinhaLista.InserirInício(11);
+
+            MinhaLista.InserirFinal(8);
+
+            MinhaLista.InserirFinal(10);
+
+            MinhaLista.InserirFinal(16);
+
+            MinhaLista.MostrarListaINIFIM();
+
+            Console.ReadKey();
+
+            Console.WriteLine("\n\n");
+
+            MinhaLista.MostrarListaFIMINI();
+
+            Console.ReadKey();
+
+            MinhaLista.Retirar(8);
+
+            Console.WriteLine("\n\n");
+
+            MinhaLista.MostrarListaFIMINI();
+
+            Console.ReadKey();
+        }
+    }
+}
